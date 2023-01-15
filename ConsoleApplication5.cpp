5afe782308183719b048560a0e316f239eadfe33
ussgordoncaptain <46254085+ussgordoncaptain@users.noreply.github.com>
2023-01-14 16:41:25 -0800

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,88 @@
+// ConsoleApplication5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+class finalValueContainer {
+public:
+    double functionReturn;
+    double var1;
+    double var2;
+    double var3;
+    double var4;
+    double var5;
+    double var6;
+    double var7;
+    finalValueContainer(double mainReturn, double a, double b, double c, double d, double e, double f, double g) {
+        functionReturn = mainReturn;
+        var1 = a;
+        var2 = b;
+        var3 = c;
+        var4 = d;
+        var5 = e;
+        var6 = f;
+        var7 = g;
+    }
+   bool operator < (finalValueContainer tester) {
+       return this->functionReturn < tester.functionReturn;
+   }
+   bool operator < (double beatThis) {
+       return this->functionReturn < beatThis;
+   }
+   ~finalValueContainer() {}
+};
+
+double arbitaryBlackBoxTest(double var1, double var2, double var3, double var4, double var5, double var6, double var7) {
+    return (var1 + var2 * var3 / (var1)+var6 * var7 / var2 + var5+var4);
+}
+
+shared_ptr <finalValueContainer> blackBoxTester( int max_step_size) {
+   int num_variables = 7; //in lisp you could dynamically increase the number of for loops please don't ask how to do this 
+  shared_ptr <finalValueContainer> output = make_shared<finalValueContainer>(0, 0, 0, 0, 0, 0, 0, 0);
+    for (int step_size = 1; step_size < max_step_size; step_size *= 2) {
+        double step_divisor = step_size * num_variables;
+        int step_quantity = step_size * num_variables;
+        for (int ii = 0; ii < step_quantity; ii++) {
+            int step_quantity2 = step_quantity - ii;
+            //keeping track of sets of step quantities because just subtracting from 1 main Q will give errors later
+            double var1 = static_cast<double>(ii) / step_divisor;
+            for (int jj = 0; jj < step_quantity2; jj++) {
+                int step_quantity3 = step_quantity2 - jj;
+                double var2 = static_cast<double>(jj) / step_divisor;
+                for (int kk = 0; kk < step_quantity3; kk++) {
+                    int step_quantity4 = step_quantity3 - kk;
+                    double var3 = static_cast<double>(kk) / step_divisor;
+                    for (int ll = 0; ll < step_quantity4; ll++) {
+                        int step_quantity5 = step_quantity4 - ll;
+                        double var4 = static_cast<double>(ll) / step_divisor;
+                        for (int mm = 0; mm < step_quantity5; mm++) {
+                            int step_quantity6 = step_quantity5 - mm;
+                            double var5 = static_cast<double>(mm) / step_divisor;
+                            for (int nn = 0; nn < step_quantity6; nn++) {
+                                int step_quantity7 = step_quantity6 - nn;
+                                double var6 = static_cast<double>(nn) / step_divisor;
+                                double var7 = static_cast<double>(step_quantity7) / step_divisor;
+                                double blackBoxTest= arbitaryBlackBoxTest(var1, var2, var3, var4, var5, var6, var7);
+                                if (*output<blackBoxTest) {
+                                    //hopefully there is no memory leak, if someone remembers how to delete zombie children other than a shared_PTR remind me
+                                    output = make_shared<finalValueContainer>(blackBoxTest, var1, var2, var3, var4, var5, var6, var7);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return output;
+}
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
